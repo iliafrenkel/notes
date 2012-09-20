6167435c1b61fa2b6be7122fb1ff6061bd07a5e6
Ilia Frenkel <frenkel.ilia@gmail.com>
2012-09-20 13:54:25 +1000

    Finished transition of code to single model.
    The purpose of this transition was to get rid of the dupplicate methods
    in NoteModel and NotesListViewModel. Now NotesListViewModel is just what
    it is supposed to be - a view that now how to display a hierarchy of
    notes starting from specified note (root note). The transition
    simplified the code and appears to be working good.

--- static/js/model.noteslist.js
+++ static/js/model.noteslist.js
@@ -55,7 +55,7 @@ function NoteModel(data) {
             note = new NoteModel(note);
         };
         note.parent(self);
-        if (opts.position) {
+        if (opts.position >= 0) {
             self.subnotes.splice(opts.position, 0, note);
         } else if ((opts.after) && (opts.after instanceof NoteModel)) {
             var idx = self.subnotes.indexOf(opts.after) + 1;
@@ -82,6 +82,7 @@ function NoteModel(data) {
      * Deletes a sub-note.
      */
     self.deleteSubnote = function(note) {
+        note.parent(null);
         self.subnotes.remove(note);
         return note;
     };
@@ -91,9 +92,9 @@ function NoteModel(data) {
      */
     self.moveSubnote = function(note, opts, parent) {
         if (parent) {
-            parent.addSubnote(self.deleteSubnote(note), opts);
+            return parent.addSubnote(self.deleteSubnote(note), opts);
         } else {
-            self.addSubnote(self.deleteSubnote(note), opts);
+            return self.addSubnote(self.deleteSubnote(note), opts);
         }
     }
     /**
@@ -198,39 +199,17 @@ function NoteModel(data) {
         //decrease the level (if possible).
         } else if (event.which == 9) {//Tab
             if (note.isZoomedIn()) return;
-            var parent = ko.contextFor(event.target).$parent;
-            if (parent instanceof NoteModel) {
-                if (event.shiftKey) {
-                    if (parent.isZoomedIn()) return false;
-                    var i = parent.subnotes.indexOf(note);
-                    parent.subnotes.splice(i,1);
-                    var grandParent = ko.contextFor($("#"+parent.id())[0]).$parent;
-                    if (grandParent instanceof NoteModel) {
-                        i = grandParent.subnotes.indexOf(parent);
-                        grandParent.subnotes.splice(i+1,0,note);
-                    } else {
-                        i = grandParent.notes.indexOf(parent);
-                        grandParent.notes.splice(i+1,0,note);
-                    }
-                } else {
-                    var i = parent.subnotes.indexOf(note)-1;
-                    if (i >= 0) {
-                        //remove the note from current position
-                        parent.subnotes.splice(i+1,1);
-                        //add it as a subnote
-                        parent.subnotes()[i].subnotes.push(note);
-                        if (!parent.subnotes()[i].isOpen()) parent.subnotes()[i].toggleOpen();
-                    }
-                }
+            var parent = note.parent();
+            var grandParent = parent.parent();
+            if (event.shiftKey) {
+                if (parent.isZoomedIn()) return false;
+                parent.moveSubnote(note, {after: parent}, grandParent);
+                $(note.hash()+" > .editor > textarea").effect("highlight", {}, 1000);
             } else {
-                if (event.shiftKey) return;
-                var i = parent.notes.indexOf(note)-1;
-                if (i >= 0) {
-                    //remove the note from current position
-                    parent.notes.splice(i+1,1);
-                    //add it as a subnote
-                    parent.notes()[i].subnotes.push(note);
-                    if (!parent.notes()[i].isOpen()) parent.notes()[i].toggleOpen();
+                if (note.prevNote()) {
+                    if (!note.prevNote().isOpen()) note.prevNote().toggleOpen();
+                    parent.moveSubnote(note, {}, note.prevNote());
+                    $(note.hash()+" > .editor > textarea").effect("highlight", {}, 1000);
                 }
             }
         //BACKSPACE
@@ -238,8 +217,7 @@ function NoteModel(data) {
         //deleted, it is marked with a property named _destroy set to true.
         } else if (event.which == 8) {//Backspace
             if ((event.target.selectionStart==event.target.selectionEnd) && (event.target.selectionEnd==0)) {
-                var notesList = ko.contextFor(event.target).$root;
-                notesList.deleteNote(note);
+                note.parent().deleteSubnote(note);
             } else {
                 return true;
             }
@@ -274,14 +252,10 @@ function NotesListViewModel(root) {
     /**
      * @method 
      */
-    self.zoomIn = function(data, event) {
-        var note;
-        if (data instanceof NoteModel) {
-            note = data;
-        } else {
-            note = data.note;
+    self.zoomIn = function(note, event) {
+        if (self.rootNote()) {
+            self.rootNote().isZoomedIn(false);
         };
-        self.rootNote().isZoomedIn(false);
         self.rootNote(note);
         note.isZoomedIn(true);
     };
@@ -319,5 +293,6 @@ function NotesListViewModel(root) {
         });
     };
    
+   if (self.rootNote()) self.zoomIn(self.rootNote());
     return this;
 };
