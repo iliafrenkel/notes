6d88d696ea648644e4abc83af8d489b6c7322ede
Ilia Frenkel <frenkel.ilia@gmail.com>
2012-10-30 17:39:09 +1100

    Started to work on issue #32.
    * Added stylesheets for 3 different font types
    * User can choose one of the three
    * Started to switch UI buttons to jQuery buttons

--- static/js/app.notes.js
+++ static/js/app.notes.js
@@ -161,6 +161,16 @@ function NotesApp() {
         $("#export-dialog").dialog("open");
         $("#export-content").select();        
     };
-
+    
+    self.setActiveStyleSheet = function(title) {
+        var i, a, main;
+        for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
+            if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title")) {
+                a.disabled = true;
+                if(a.getAttribute("title") == title) a.disabled = false;
+            }
+        }
+    };
+    
     return self;
 };
